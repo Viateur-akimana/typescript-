ea513f75ef5e946c708d63db8a0c1a7bd5a6c13d
akimana-viateur <akimanaviateur94@gmail.com>
2023-08-29 17:38:18 +0200

    intial commit

--- typescript-crash.ts
+++ typescript-crash.ts
@@ -6,3 +6,103 @@ let x: any = 'Hello'
 
 let ids: number[] = [1, 2, 3, 4, 5]
 let arr: any[] = [1, true, 'Hello']
+
+// Tuple
+let person: [number, string, boolean] = [1, 'Brad', true]
+// Tuple Array
+let employees: [number, string][]
+
+employee = [
+  [1, 'Brad'],
+  [2, 'John'],
+  [3, 'Jill'],
+]
+
+// Union
+let pid: string | number
+pid = '22'
+
+// Enum
+enum Direction1 {
+  Up = 1,
+  Down,
+  Left,
+  Right,
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
